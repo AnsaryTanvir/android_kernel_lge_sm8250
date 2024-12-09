d16e3bc155d5e42404d47196e1515a5fee86aa11
AnsaryTanvir <109978583+AnsaryTanvir@users.noreply.github.com>
2024-12-09 10:46:29 -0500

    Update gcc-wrapper.py

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -1,11 +1,8 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) 2011-2017, 2018 The Linux Foundation. All rights reserved.
 
 # -*- coding: utf-8 -*-
-
-# Invoke gcc, looking for warnings, and causing a failure if there are
-# non-whitelisted warnings.
 
 import errno
 import re
@@ -13,27 +10,22 @@ import os
 import sys
 import subprocess
 
-# Note that gcc uses unicode, which may depend on the locale.  TODO:
-# force LANG to be set to en_US.UTF-8 to get consistent warnings.
-
 allowed_warnings = set([
     "umid.c:138",
     "umid.c:213",
     "umid.c:388",
- ])
+])
 
-# Capture the name of the object file, can find it.
 ofile = None
 
 warning_re = re.compile(r'''(.*/|)([^/]+\.[a-z]+:\d+):(\d+:)? warning:''')
+
 def interpret_warning(line):
-    """Decode the message from gcc.  The messages we care about have a filename, and a warning"""
+    """Decode the message from gcc."""
     line = line.rstrip('\n')
     m = warning_re.match(line)
     if m and m.group(2) not in allowed_warnings:
-        print "error, forbidden warning:", m.group(2)
-
-        # If there is a warning, remove any object if it exists.
+        print("error, forbidden warning:", m.group(2))
         if ofile:
             try:
                 os.remove(ofile)
@@ -43,7 +35,6 @@ def interpret_warning(line):
 
 def run_gcc():
     args = sys.argv[1:]
-    # Look for -o
     try:
         i = args.index('-o')
         global ofile
@@ -54,19 +45,18 @@ def run_gcc():
     compiler = sys.argv[0]
 
     try:
-        proc = subprocess.Popen(args, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(args, stderr=subprocess.PIPE, text=True)
         for line in proc.stderr:
-            print line,
+            print(line, end="")
             interpret_warning(line)
-
         result = proc.wait()
     except OSError as e:
         result = e.errno
         if result == errno.ENOENT:
-            print args[0] + ':',e.strerror
-            print 'Is your PATH set correctly?'
+            print(args[0] + ':', e.strerror)
+            print('Is your PATH set correctly?')
         else:
-            print ' '.join(args), str(e)
+            print(' '.join(args), str(e))
 
     return result
 
